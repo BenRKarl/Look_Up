2712e7045009d52e500c73f81f207212a5964882
Ben Karl <benrkarl@gmail.com>
2014-07-16 10:48:53 -0400

    first passing test ensures model has appropriate instance variable

--- spec/models/astronomy_spec.rb
+++ spec/models/astronomy_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Astronomy, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it 'initializes with an options instance variable' do
+    astronomy = Astronomy.new({this: 'is in the @options int. variable'})
+    actual = astronomy.instance_variable_names[0]
+    expected = '@options'
+    expect(actual). to eq(expected)
+  end
+
 end
